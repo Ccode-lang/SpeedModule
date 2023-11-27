d20b0727446f4119e7a500366632b5c95ebf5715
Ccode-lang <coop112097@gmail.com>
2023-11-27 10:26:13 -0700

    Cyclops module registered.  No functionality yet.

---
+++ SeamothSpeedModule/Items/Handlers/CyclopsSpeedModuleHandler.cs
@@ -0,0 +1,18 @@
+﻿using MoreCyclopsUpgrades.API.Upgrades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedModule.Items.Handlers
+{
+    internal class CyclopsSpeedModuleHandler : UpgradeHandler
+    {
+        public CyclopsSpeedModuleHandler(TechType speedModule, SubRoot cyclops) : base(speedModule, cyclops)
+        {
+            this.OnClearUpgrades = () => { };
+            this.OnUpgradeCounted = () => { };
+        }
+    }
+}
---
+++ SeamothSpeedModule/Items/Modules/CyclopsSpeedModule.cs
@@ -0,0 +1,46 @@
+﻿using MoreCyclopsUpgrades.API.Upgrades;
+using Nautilus.Crafting;
+using Nautilus.Handlers;
+using System.Collections;
+using UnityEngine;
+
+namespace SpeedModule.Items.Modules
+{
+    internal class CyclopsSpeedModule : CyclopsUpgrade
+    {
+        public override CraftTree.Type FabricatorType { get; } = CraftTree.Type.CyclopsFabricator;
+        public override IEnumerator GetGameObjectAsync(IOut<GameObject> gameObject)
+        {
+            TaskResult<GameObject> task = new TaskResult<GameObject>();
+            yield return base.GetGameObjectAsync(task);
+            GameObject obj = task.Get();
+            gameObject.Set(obj);
+            yield break;
+        }
+        public override TechType RequiredForUnlock { get; } = TechType.Cyclops;
+        public override string[] StepsToFabricatorTab { get; } = new string[]
+        {
+            "CyclopsMenu"
+        };
+
+        protected override RecipeData GetBlueprintRecipe()
+        {
+            return new RecipeData
+            {
+                craftAmount = 1,
+                Ingredients =
+                {
+                    new CraftData.Ingredient(TechType.Titanium, 6),
+                }
+            };
+        }
+        public override float CraftingTime
+        {
+            get
+            {
+                return 12f;
+            }
+        }
+        internal CyclopsSpeedModule() : base("CyclopsSpeedModule", "Cyclops Speed Module", "Make your cyclops go 2 times faster.") {}
+    }
+}
